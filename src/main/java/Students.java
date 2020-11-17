11ebb95d28d2f61a7c21db28bf49d54ee33cacd1
student119 <37659831+student119@users.noreply.github.com>
2020-11-17 15:18:05 +0000

    Comments Finalised For Now

--- src/main/java/Students.java
+++ src/main/java/Students.java
@@ -5,92 +5,129 @@ public class Students
 
 {
 
+    // Declares a String named studentname.
     String studentName;
+    // Declares an integer named studentAge.
     int studentAge;
+    // Declares a DateTime named studentDOB.
     DateTime studentDOB;
+    // Declares an integer named studentID.
     int studentID;
+    // Declares a String named studentUsername.
     String studentUsername;
+    // Declares an ArrayList of type String named studentCourses and sets it's initial capacity to 1.
     ArrayList <String> studentCourses = new ArrayList <String> (1);
+    // Declares an ArrayList of type String named studentModules and sets it's initial capacity to 10.
     ArrayList <String> studentModules = new ArrayList <String> (10);
 
+    // Constructor Method of Students with parameters of :
+    // String named name;
+    // Integer named age;
+    // DateTime named DOB;
+    // Integer named id;
+    // ArrayList of type String named courses;
+    // ArrayList of type String named modules.
     public Students (String name, int age, DateTime DOB, int id, ArrayList <String> courses, ArrayList <String> modules)
 
     {
 
+        // Calls the setter method named setStudentName and passes name as it's parameter.
         setStudentName (name);
+        // Calls the setter method named setStudentAge and passes name as it's parameter.
         setStudentAge (age);
+        // Calls the setter method named setStudentDOB and passes DOB as it's parameter.
         setStudentDOB (DOB);
+        // Calls the setter method named setStudentID and passes id as it's parameter.
         setStudentID (id);
         // setStudentUsername (name, age);
+        // Calls the setter method named setStudentCourses and passes courses as it's parameter.
         setStudentCourses (courses);
+        // Calls the setter method named setStudentModules and passes modules as it's parameter.
         setStudentModules (modules);
 
     }
 
+    // Method of type String named getStudentName.
     public String getStudentName ()
 
     {
 
+        // Returns studentName when called.
         return studentName;
 
     }
 
+    // Method of type void named setStudentName with a String named studentName as it's parameter.
     public void setStudentName (String studentName)
 
     {
 
+        // studentName is set equal to studentName.
         this.studentName = studentName;
 
     }
 
+    // Method of type integer named getStudentAge.
     public int getStudentAge ()
 
     {
 
+        // Returns studentAge when called.
         return studentAge;
 
     }
 
+    // Method of type void named setStudentAge with an integer named studentAge as it's parameter.
     public void setStudentAge (int studentAge)
 
     {
 
+        // studentAge is set equal to studentAge.
         this.studentAge = studentAge;
 
     }
 
+    // Method of type DateTime named getStudentDOB.
     public DateTime getStudentDOB ()
 
     {
 
+        // Returns studentDOB when called.
         return studentDOB;
 
     }
 
+    // Method of type void named setStudentDOB with a DateTime named studentDOB as it's parameter.
     public void setStudentDOB (DateTime studentDOB)
 
     {
 
+        // studentDOB is set equal to studentDOB.
         this.studentDOB = studentDOB;
 
     }
 
+    // Method of type integer named getStudentID.
     public int getStudentID ()
 
     {
 
+        // Returns studentID when called.
         return studentID;
 
     }
 
+    // Method of type void named setStudentID with an integer named studentID as it's parameter.
     public void setStudentID (int studentID)
 
     {
 
+        // studentID is set equal to studentID.
         this.studentID = studentID;
 
     }
 
+    // Method of type String named getStudentUsername.
     public String getStudentUsername ()
 
     {
@@ -98,8 +135,10 @@ public class Students
         // System.out.println(getStudentName());
         // System.out.println(getStudentAge());
 
+        // studentUsername is set equal to the concatenated values of studentName and the String value of studentAge.
         this.studentUsername = getStudentName().concat (String.valueOf (getStudentAge ()));
 
+        // Returns studentUsername when called.
         return studentUsername;
 
     }
@@ -112,34 +151,42 @@ public class Students
 //
 //    }
 
+    // Method of type ArrayList of type String named getStudentCourses.
     public ArrayList <String> getStudentCourses ()
 
     {
 
+        // Return studentCourses when called.
         return studentCourses;
 
     }
 
+    // Method of type void named setStudentCourses with an ArrayList of type String named studentCourses as it's parameter.
     public void setStudentCourses (ArrayList <String> studentCourses)
 
     {
 
+        // studentCourses is set equal to studentCourses.
         this.studentCourses = studentCourses;
 
     }
 
+    // Method of type ArrayList of type String named getStudentModules.
     public ArrayList <String> getStudentModules ()
 
     {
 
+        // Returns studentModules when called.
         return studentModules;
 
     }
 
+    // Method of type void named setStudentModules with an ArrayList of type String named studentModules as it's parameter.
     public void setStudentModules (ArrayList <String> studentModules)
 
     {
 
+        // studentModules is set equal to studentModules.
         this.studentModules = studentModules;
 
     }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -14,7 +14,7 @@ public class StudentTest
     private Students testStudent;
     // Declares a private String named testName and sets it equal to "Craig Walsh".
     private String testName = "Craig Walsh";
-    // Declares a private int named testAge and sets it equal to 28.
+    // Declares a private integer named testAge and sets it equal to 28.
     private int testAge = 28;
     // Declares a private DateTime named testDOB and sets it equal to (1992, 1, 7, 13, 49).
     private DateTime testDOB = new DateTime (1992, 1, 7, 13, 49);
@@ -34,72 +34,103 @@ public class StudentTest
 
     {
 
-        //
+        // testStudent has testName, testAge. testDOB, testID, testCourses, and testModules passed as it's parameters.
         testStudent = new Students (testName, testAge, testDOB, testID, testCourses, testModules);
-        // 
+        // The testUsername is built by concatenating the name and age of the student.
+        // It does not have a setter method, instead using a modified getter method to generate the username.
+        // For testing purposes, the username must be generated using the below line.
+        // testUsername is set equal to the concatenated values of testName and the String value of testAge.
         testUsername = testName.concat((String.valueOf (testAge)));
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentNameTest which will test if the name for a student has been correctly assigned.
     public void studentNameTest ()
 
     {
 
+        // assertEquals is a method from junit which compares the result from the testStudent getStudentName getter method from Students
+        // and sees if it matches the value in testName.
         assertEquals (testStudent.getStudentName (), testName);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentAgeTest which will test if the age for a student has been correctly assigned.
     public void studentAgeTest ()
 
     {
 
+        // assertsEquals is a method from junit which compares the result from the testStudent getStudentAge getter method from Students
+        // and sees if it matches the value in testAge.
         assertNotNull (String.valueOf (testStudent.getStudentAge ()), testAge);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentDOBTest which will test if the date of birth for a student has been correctly assigned.
     public void studentDOBTest ()
 
     {
 
+        // assertEquals is a method from junit which compares the result from the testStudent getStudentDOB getter method from Students
+        // and sees if it matches the value in testDOB.
         assertEquals (testStudent.getStudentDOB (), testDOB);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentIDTest which will test if the id for a student has been correctly assigned.
     public void studentIDTest ()
 
     {
 
+        // assertNotNull is a method from junit which compares the result from the testStudent getStudentID getter method from Students
+        // and sees if it matches the value in testID.
         assertNotNull (String.valueOf (testStudent.getStudentID ()), testID);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentUsernameTest which will test if the username for a student has been correctly assigned.
     public void studentUsernameTest ()
 
     {
 
+        // assertEquals is a method from junit which compares the result from the testStudent getStudentUsername getter method from Students
+        // and sees if it matches the value in testUsername.
         assertEquals (testStudent.getStudentUsername (), testUsername);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentCourseTest which will test if the course for a student has been assigned correctly.
     public void studentCoursesTest ()
 
     {
 
+        // assertEquals is a method from junit which compares the results from the testStudents getStudentCourses getter method from Students
+        // and sees if it matches the value in testCourses.
         assertEquals (testStudent.getStudentCourses (), testCourses);
 
     }
 
+    // @Test test to run for junit.
     @Test
+    // Method of type void named studentModulesTest which will test if the modules for a student have been correctly assigned.
     public void studentModulesTest ()
 
     {
 
+        // assertEquals is a method from junit which compares the results from the testStudent getStudentModules getter method from Students
+        // and sees if it matches the value in testModules.
         assertEquals (testStudent.getStudentModules (), testModules);
 
     }
